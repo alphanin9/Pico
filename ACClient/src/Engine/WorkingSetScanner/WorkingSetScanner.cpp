c34febd11a4c000b03abcaed0bbd6092daf89311
alphanin9 <47785076+alphanin9@users.noreply.github.com>
2025-01-14 01:06:16 +0200

    Working set tinkering...

--- ACClient/src/Engine/WorkingSetScanner/WorkingSetScanner.cpp
+++ ACClient/src/Engine/WorkingSetScanner/WorkingSetScanner.cpp
@@ -4,8 +4,6 @@
 
 void pico::Engine::WorkingSetScanner::Tick() noexcept
 {
-    
-
     // Time between working set updates, as querying the working set every frame is expensive
     constexpr std::chrono::seconds WorkingSetUpdateTime{5};
 
@@ -13,7 +11,8 @@ void pico::Engine::WorkingSetScanner::Tick() noexcept
     auto& logger = Logger::GetLogSink();
 
     // A potential optimization could be caching the addresses of the allocations we have already walked and skipping
-    // them in the WS. Keep in mind the main performance hog is the querying of the process working set, everything else takes very little time.
+    // them in the WS. Keep in mind the main performance hog is the querying of the process working set, everything else
+    // takes very little time.
 
     const auto timestamp = std::chrono::high_resolution_clock::now();
 
@@ -24,6 +23,7 @@ void pico::Engine::WorkingSetScanner::Tick() noexcept
         // Pray this doesn't race lol
         s_engine.m_threadsUnderHeavyLoad++;
         m_workingSetCache = shared::MemoryEnv::GetProcessWorkingSet();
+        // m_scannedPages.clear();
         // Can wait for completion now, the rest doesn't take much
         s_engine.m_threadsUnderHeavyLoad--;
         const auto taken =
@@ -34,60 +34,86 @@ void pico::Engine::WorkingSetScanner::Tick() noexcept
         m_nextWorkingSetCacheUpdate = timestamp + WorkingSetUpdateTime;
 
         WalkWorkingSet();
-        WalkSections();
     }
+}
+
+void pico::Engine::WorkingSetScanner::ProcessPage(Windows::MEMORY_WORKING_SET_BLOCK aBlock) noexcept
+{
+    // Note: figure out how to scan shared pages in a more optimized fashion!
+    // Maybe abandon working set completely and just walk VirtualQuery results?
+
+    auto& logger = Logger::GetLogSink();
+
+    // Note: hack for lazier mappers (check for shared section permissions being RWX), still should optimize it
+    // I mean, what if there's some legit image with RWX image sections that triggers lots of VirtualQuery calls and
+    // lags the app to a standstill?
+    if (aBlock.Shared == 1 && (aBlock.Protection & (1u << 2u)) == 0u)
+    {
+        return;
+    }
+
+    // Transform VPN into actual virtual address for the page
+    const auto pageAddr = reinterpret_cast<void*>(aBlock.VirtualPage * 0x1000);
+
+    MEMORY_BASIC_INFORMATION pageInfo{};
+
+    // Might have deallocated or the like...
+    if (!VirtualQuery(pageAddr, &pageInfo, sizeof(pageInfo)))
+    {
+        return;
+    }
+
+    // Note: all non-executable pages are skipped anyway
+    if (!shared::MemoryEnv::IsProtectionExecutable(pageInfo.Protect) ||
+        !shared::MemoryEnv::IsProtectionExecutable(pageInfo.AllocationProtect))
+    {
+        logger->warn("Execution status at page {} has been altered! Protection: {:#x}, alloc protect: {:#x}, "
+                     "page type: {:#x}, base address: {}, size: {:#x}",
+                     pageAddr, pageInfo.Protect, pageInfo.AllocationProtect, pageInfo.Type, pageInfo.BaseAddress,
+                     pageInfo.RegionSize);
+    }
+
+    if (pageInfo.Type != MEM_IMAGE && pageInfo.RegionSize >= MaxExecutableAllocationSize)
+    {
+        logger->error("Executable addr {} is not image and is big! Protection: {:#x}, alloc protect: {:#x}, page "
+                      "type: {:#x}, "
+                      "size: {:#x}, base address: {}, state: {:#x}",
+                      pageAddr, pageInfo.Protect, pageInfo.AllocationProtect, pageInfo.Type, pageInfo.RegionSize,
+                      pageInfo.BaseAddress, pageInfo.State);
+    }
+
+    // Math should be OK
+    // const auto basePage = reinterpret_cast<pico::Uint64>(pageInfo.BaseAddress) / 0x1000u;
+    // const auto finalPage = basePage + (pageInfo.RegionSize / 0x1000u);
+
+    // for (auto i = basePage; i <= finalPage; i++)
+    //{
+    //     m_scannedPages.insert(i);
+    // }
 }
 
 void pico::Engine::WorkingSetScanner::WalkWorkingSet() noexcept
 {
-    // Note: In CS2, 0x2000 will also catch trampolines made by GameOverlayRenderer64.dll
-    // The maximum trampoline allocation size by overlay renderer seems to be 0x10000u
-    // So, make it 0x10001u? No, less safe for small binaries
-    constexpr auto MaxExecutableAllocationSize = 0x2000u;
-
     auto& logger = Logger::GetLogSink();
+
+    const auto start = std::chrono::high_resolution_clock::now();
 
     for (auto workingSetEntry : m_workingSetCache)
     {
-        // Transform the VPN into an actual address
-        const auto pageAddr = reinterpret_cast<void*>(workingSetEntry.VirtualPage * 0x1000);
-
-        // Skip non-executable pages and pages that belong to a section, we will scan sections later
-        if ((workingSetEntry.Protection & (1u << 1u)) == 0u || workingSetEntry.Shared == 1)
+        // Skip non-executable pages
+        if ((workingSetEntry.Protection & (1u << 1u)) == 0u)
         {
             continue;
         }
 
-        MEMORY_BASIC_INFORMATION pageInfo{};
-
-        // Might have deallocated or the like...
-        if (!VirtualQuery(pageAddr, &pageInfo, sizeof(pageInfo)))
-        {
-            continue;
-        }
-
-        if (!shared::MemoryEnv::IsProtectionExecutable(pageInfo.Protect))
-        {
-            logger->warn("Executable in WS addr {} is no longer executable! Protection: {:#x}, alloc protect: {:#x}, "
-                         "page type: {:#x}, size: {:#x}",
-                         pageAddr, pageInfo.Protect, pageInfo.AllocationProtect, pageInfo.Type, pageInfo.RegionSize);
-        }
-
-        // MEM_IMAGE check might be useless here, not sure
-        if (pageInfo.Type != MEM_IMAGE && pageInfo.RegionSize >= MaxExecutableAllocationSize)
-        {
-            logger->error("Executable addr {} is not image and is big! Protection: {:#x}, alloc protect: {:#x}, page "
-                          "type: {:#x}, "
-                          "size: {:#x}, base address: {}, state: {:#x}",
-                          pageAddr, pageInfo.Protect, pageInfo.AllocationProtect, pageInfo.Type, pageInfo.RegionSize,
-                          pageInfo.AllocationBase, pageInfo.State);
-        }
+        ProcessPage(workingSetEntry);
     }
-}
 
-void pico::Engine::WorkingSetScanner::WalkSections() noexcept
-{
-    // TODO
+    const auto taken =
+        std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start)
+            .count();
+
+    logger->info("Time to walk working set: {}ms", taken);
 }
 
 pico::Engine::WorkingSetScanner& pico::Engine::WorkingSetScanner::Get() noexcept
--- ACClient/src/Engine/WorkingSetScanner/WorkingSetScanner.hpp
+++ ACClient/src/Engine/WorkingSetScanner/WorkingSetScanner.hpp
@@ -11,16 +11,24 @@ namespace pico::Engine
  * We then proceed to walk it to find executable pages. If a page is executable, we call VirtualQuery on it to obtain
  * whether or not the page belongs to a section, along with its allocation size.
  *
- * If the page does not belong to a section and its allocation size is larger than 2kB, we log the offending memory region. In production, we should also send
- * the offending pages to the backend for further analysis. This may cause false positives with detour trampolines.
+ * If the page does not belong to a section and its allocation size is larger than 2kB, we log the offending memory
+ * region. In production, we should also send the offending pages to the backend for further analysis. This may cause
+ * false positives with detour trampolines.
  *
  * Note that this may fail if the image has a kernel mode driver attempting to hide it with VAD manipulation/whatnot.
  */
 struct WorkingSetScanner : public shared::Util::NonCopyableOrMovable
 {
+    // The maximum size of a non-image executable allocation before we log it.
+    static constexpr auto MaxExecutableAllocationSize = 0x2000u;
+
     // A cache of the process working set.
     // Although non-executable entries might update quite often, executable ones will update much rarer.
     pico::Vector<Windows::MEMORY_WORKING_SET_BLOCK> m_workingSetCache{};
+
+    // A set of already iterated virtual page numbers by VirtualQuery
+    // pico::Set<pico::Uint64> m_scannedPages{};
+
     std::chrono::high_resolution_clock::time_point m_nextWorkingSetCacheUpdate{};
 
     /**
@@ -29,9 +37,14 @@ struct WorkingSetScanner : public shared::Util::NonCopyableOrMovable
     void WalkWorkingSet() noexcept;
 
     /**
-     * \brief Walks the process's shared memory (section objects) in search of potentially badly loaded libraries.
+     * \brief Processes a page in search for anomalous characteristics.
+     *
+     * This can probably be optimized in more ways than it currently is. Currently we only scan for private pages
+     * instead of handling shared ones as well.
+     *
+     * \param aBlock The working set entry block. Should be executable.
      */
-    void WalkSections() noexcept;
+    void ProcessPage(Windows::MEMORY_WORKING_SET_BLOCK aBlock) noexcept;
 
     /**
      * \brief Ticks component in the thread pool worker.
